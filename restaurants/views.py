07b1cd7b01002bd7f72cc55b68fcc50e4f59f76d
fisal77 <fisal0707@gmail.com>
2019-07-22 21:41:55 +0300

    Fix :bug:

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,7 +6,11 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
-
+    	'my_list': [
+    		{"restaurant_name": "Al-Baik", "food_type": "Checken Spicy"},
+    		{"restaurant_name": "Maestro Pizza", "food_type": "Vegtibales pizza"},
+    		{"restaurant_name": "Al-Romansiyah", "food_type": "Kabsah ;)"},
+    	],
     }
     return render(request, 'list.html', context)
 
@@ -14,6 +18,6 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
-
+    	'my_object': {"restaurant_name": "Al-Baik", "food_type": "Checken Spicy"}
     }
     return render(request, 'detail.html', context)
